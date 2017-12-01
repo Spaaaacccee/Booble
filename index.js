71af79a31485649367c7c2ad119a6d415c6f525e
ZHENG <ZHE0104@balwynhs.vic.edu.au>
2017-12-01 21:51:02 +1100

    changed port to 8080

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000)
+app.listen(8080)
 
 app.use(express.static(__dirname))
 
